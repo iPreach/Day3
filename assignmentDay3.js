9e5cefe5b3b16bef15d5697a169872a69f3dc89f
Kraig De Leon Diedrick <47286976+iPreach@users.noreply.github.com>
2022-02-15 19:07:03 -0500

    Add files via upload

---
+++ assignmentDay3.js
@@ -0,0 +1,229 @@
+for(var i=5; i<=50; i+=5)
+{
+    console.log(i);
+}
+
+
+var english = 75;
+var math = 98;
+var science = 85;
+var computer = 62;
+var practicum = 31;
+
+var grade = english;
+
+if(grade <= 100 && grade >= 90)
+{
+    console.log(grade, "A+");
+}
+else if(grade < 90 && grade >= 85)
+{
+    console.log(grade, "A");
+}
+else if(grade < 85 && grade >= 80)
+{
+    console.log(grade, "A-");
+}
+else if(grade < 80 && grade >= 75)
+{
+    console.log(grade,"B+");
+}
+else if(grade < 75 && grade >= 70)
+{
+    console.log(grade, "B");
+}
+else if(grade < 70 && grade >=65)
+{
+    console.log(grade, "B-");
+}
+else if(grade < 65 && grade >=60)
+{
+    console.log(grade, "C+");
+}
+else if(grade < 60 && grade >= 55)
+{
+    console.log(grade, "C");
+}
+else if(grade < 55 && grade >= 50)
+{
+    console.log(grade, "C-");
+}
+else
+{
+    console.log(grade, "FAIL");
+}
+
+var grade = math;
+
+if(grade <= 100 && grade >= 90)
+{
+    console.log(grade, "A+");
+}
+else if(grade < 90 && grade >= 85)
+{
+    console.log(grade, "A");
+}
+else if(grade < 85 && grade >= 80)
+{
+    console.log(grade, "A-");
+}
+else if(grade < 80 && grade >= 75)
+{
+    console.log(grade,"B+");
+}
+else if(grade < 75 && grade >= 70)
+{
+    console.log(grade, "B");
+}
+else if(grade < 70 && grade >=65)
+{
+    console.log(grade, "B-");
+}
+else if(grade < 65 && grade >=60)
+{
+    console.log(grade, "C+");
+}
+else if(grade < 60 && grade >= 55)
+{
+    console.log(grade, "C");
+}
+else if(grade < 55 && grade >= 50)
+{
+    console.log(grade, "C-");
+}
+else
+{
+    console.log(grade, "FAIL");
+}
+
+
+var grade = science;
+
+if(grade <= 100 && grade >= 90)
+{
+    console.log(grade, "A+");
+}
+else if(grade < 90 && grade >= 85)
+{
+    console.log(grade, "A");
+}
+else if(grade < 85 && grade >= 80)
+{
+    console.log(grade, "A-");
+}
+else if(grade < 80 && grade >= 75)
+{
+    console.log(grade,"B+");
+}
+else if(grade < 75 && grade >= 70)
+{
+    console.log(grade, "B");
+}
+else if(grade < 70 && grade >=65)
+{
+    console.log(grade, "B-");
+}
+else if(grade < 65 && grade >=60)
+{
+    console.log(grade, "C+");
+}
+else if(grade < 60 && grade >= 55)
+{
+    console.log(grade, "C");
+}
+else if(grade < 55 && grade >= 50)
+{
+    console.log(grade, "C-");
+}
+else
+{
+    console.log(grade, "FAIL");
+}
+
+
+var grade = computer;
+
+if(grade <= 100 && grade >= 90)
+{
+    console.log(grade, "A+");
+}
+else if(grade < 90 && grade >= 85)
+{
+    console.log(grade, "A");
+}
+else if(grade < 85 && grade >= 80)
+{
+    console.log(grade, "A-");
+}
+else if(grade < 80 && grade >= 75)
+{
+    console.log(grade,"B+");
+}
+else if(grade < 75 && grade >= 70)
+{
+    console.log(grade, "B");
+}
+else if(grade < 70 && grade >=65)
+{
+    console.log(grade, "B-");
+}
+else if(grade < 65 && grade >=60)
+{
+    console.log(grade, "C+");
+}
+else if(grade < 60 && grade >= 55)
+{
+    console.log(grade, "C");
+}
+else if(grade < 55 && grade >= 50)
+{
+    console.log(grade, "C-");
+}
+else
+{
+    console.log(grade, "FAIL");
+}
+
+
+var grade = practicum;
+
+if(grade <= 100 && grade >= 90)
+{
+    console.log(grade, "A+");
+}
+else if(grade < 90 && grade >= 85)
+{
+    console.log(grade, "A");
+}
+else if(grade < 85 && grade >= 80)
+{
+    console.log(grade, "A-");
+}
+else if(grade < 80 && grade >= 75)
+{
+    console.log(grade,"B+");
+}
+else if(grade < 75 && grade >= 70)
+{
+    console.log(grade, "B");
+}
+else if(grade < 70 && grade >=65)
+{
+    console.log(grade, "B-");
+}
+else if(grade < 65 && grade >=60)
+{
+    console.log(grade, "C+");
+}
+else if(grade < 60 && grade >= 55)
+{
+    console.log(grade, "C");
+}
+else if(grade < 55 && grade >= 50)
+{
+    console.log(grade, "C-");
+}
+else
+{
+    console.log(grade, "FAIL");
+}
